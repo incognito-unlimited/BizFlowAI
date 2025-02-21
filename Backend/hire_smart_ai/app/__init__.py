d4fdc06edcc35500cb633d604b77a18d1f1c301d
Suyash sahu <142486268+7uyash@users.noreply.github.com>
2025-02-22 02:08:15 +0530

    Delete Backend directory

--- Backend/hire_smart_ai/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-    from .routes import job_routes, candidate_routes
-    app.register_blueprint(job_routes.bp)
-    app.register_blueprint(candidate_routes.bp)
-    return app
