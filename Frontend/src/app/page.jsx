eeb8e071aaafd1923cd758c88a9a1b0e23b358c3
ChiragBhal <159833490+chirahbhal312@users.noreply.github.com>
2025-02-22 13:07:48 +0530

    Update page.jsx

--- Frontend/src/app/page.jsx
+++ Frontend/src/app/page.jsx
@@ -11,7 +11,6 @@ export default function Home() {
     <div className="home_container p-10">
     <h1 className="text-center text-3xl font-montserrat font-bold text-white/70">New Product Analysis</h1>
     <ProductForm/>
-    {/* <AnalysisContainer/> */}
     </div>
     </div>
   );
